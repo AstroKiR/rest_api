9508e1b66bc23186a3e8991a02e6ddfd73b2a112
AstroKiR <eu1kp@yahoo.com>
2021-08-09 23:36:04 +0300

    first route

--- main.go
+++ main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func home(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "<h1>Run Successfully!</h1>")
+}
 
 func main() {
-	fmt.Println("rest api")
+	router := mux.NewRouter().StrictSlash(true)
+	router.HandleFunc("/", home)
+	log.Fatal(http.ListenAndServe(":8080", router))
 }
