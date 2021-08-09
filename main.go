5e2f132d25222857f53df3e218344d33dfabf63f
AstroKiR <eu1kp@yahoo.com>
2021-08-10 00:01:57 +0300

    dummy database

--- main.go
+++ main.go
@@ -8,6 +8,25 @@ import (
 	"github.com/gorilla/mux"
 )
 
+// A global variable that is incremented everytime a book is added.
+// Used for providing a unique ID to each book
+var count int
+
+type Book struct {
+	ID     int     `json:"id"`
+	Isdn   string  `json:"isdn"`
+	Title  string  `json:"title"`
+	Author *Author `json:"author"`
+}
+
+type Author struct {
+	FirstName string `json:"firstname"`
+	LastName  string `json:"lastname"`
+}
+
+// A slice that will contain the books
+var books []Book
+
 func home(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "<h1>Run Successfully!</h1>")
 }
